471744733b2dc56739fa7db0bb36017b950514fb
Muharrem Rahat <atomtr42@gmail.com>
2022-01-06 08:30:59 +0300

    Proje dosyası ekle.

---
+++ Diziler_Ornek_3/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diziler_Ornek_3
+{
+    public class Class1
+    {
+        public string arananSehir;
+        public int sayac;
+        public string[] sehirler = new string[10];
+        public int şehir;
+    }
+}
---
+++ Diziler_Ornek_3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diziler_Ornek_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 a = new Class1();
+
+            Console.WriteLine("Gireceğiniz şehir sayısını giriniz : ");
+            a.şehir = Convert.ToInt32(Console.ReadLine());
+            a.sehirler = new string[a.şehir];
+
+            for (int i = 0; i < a.sehirler.Length; i++)
+            {
+                Console.Write("{0}. Şehri Girin : ", i + 1);
+                a.sehirler[i] = Console.ReadLine();
+            }
+            Console.WriteLine();
+
+            Console.Write("Aranacak şehri girin : ");
+            a.arananSehir = Console.ReadLine();
+
+            for (int i = 0; i < a.sehirler.Length; i++)
+            {
+                if (a.sehirler[i] == a.arananSehir)
+                {
+                    a.sayac++;
+                }
+            }
+
+            if (a.sayac == 0)
+            {
+                Console.WriteLine("Şehir bulunamadı.");
+            }
+            else
+            {
+                Console.WriteLine("Dizi içinde {0} adet {1} şehrine ait kayıt mevcut ", a.sayac, a.arananSehir);
+            }
+            Console.ReadKey();
+        }
+    }
+}
